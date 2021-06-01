51be00bd87ac1525e6799efe52ebb0ecf2f0c68d
ralf-boltshauser <ra.boltshauser3@gmail.com>
2021-06-01 11:04:22 +0200

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 ### PAKETIERE LOKAL
 
 # db dump via mysql docker instanz
-id=68cec3feb83a # 68cec3feb83a
-docker exec -ti $id /usr/bin/mysqldump -u ralf -p1234 -d wordpress > dump.sql
+id=6229f1fee664 # 68cec3feb83a
+docker exec -ti 6229f1fee664 /usr/bin/mysqldump -u root -psomewordpress -d wordpress > dump.sql
 
 tar -C ./wordpress/wp-content/ -cvzf uploads.tar.gz ./uploads
 
@@ -14,9 +14,5 @@ tar .C ./wordpress/wp-content -cvzf plugins.tar.gz ./plugins
 
 docker exec -ti wpdeploy_wordpress_1 cat wp-config.php > wp-config.localdev.php
 
-scp themes.tar.gz kx84u_bbw@5ia18a.root-systems.ch:~/sites/5ia18a.root-systems.ch/boltshauser/
-scp uploads.tar.gz kx84u_bbw@5ia18a.root-systems.ch:~/sites/5ia18a.root-systems.ch/boltshauser/
-scp plugins.tar.gz kx84u_bbw@5ia18a.root-systems.ch:~/sites/5ia18a.root-systems.ch/boltshauser/
-scp dump.sql kx84u_bbw@5ia18a.root-systems.ch:~/sites/5ia18a.root-systems.ch/boltshauser/ 
+ssh kx84u_bbw@5ia18a.root-systems.ch 'cd sites/5ia18a.root-systems.ch/boltshauser ; git clone https://github.com/ralf-boltshauser/wpdeploy.git ; mkdir themes ; mkdir plugins ; mkdir uploads ; tar -xf ./wpdeploy/themes.tar.gz -C ./ ; tar -xf ./wpdeploy/plugins.tar.gz -C ./ ; tar -xf ./wpdeploy/uploads.tar.gz -C ./ ; cp ./wpdeploy/dump.sql ./dump.sql'
 
-ssh kx84u_bbw@5ia18a.root-systems.ch 'tar -xvf ~/sites/5ia18a.root-systems.ch/boltshauser/themes.tar.gz'
